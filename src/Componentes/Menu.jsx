70cf291c15ffa271892885ab51c8ce8274b64dfd
websysnet <ironpiero1@gmail.com>
2025-12-13 09:59:18 -0300

    menu responsivo

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -1,43 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menu.css'
+
 export default function Menu() {
+    const [open, setOpen] = useState(false)
+
     return (
         <div>
             <nav className='menu'>
-                    
-                    <img className='logo' src="/imagenes/logotalents.png" alt="Talento Sports" />
-               
+                <img className='logo' src="/imagenes/logotalents.png" alt="Talento Sports" />
+
+                <button
+                    className='menu-toggle'
+                    aria-expanded={open}
+                    aria-label='Toggle menu'
+                    onClick={() => setOpen(!open)}
+                >
+                    ☰
+                </button>
+
+                <ul className={`menu-links ${open ? 'open' : ''}`}>
                     <li>
                         <a href="/">🏠 Home</a>
                     </li>
                     <li>
                         <a href="/nosotros">👥 Nosotros</a>
-
                     </li>
                     <li>
                         <a href="/deportes">⚽ Deportes</a>
-
                     </li>
-                     <li>
+                    <li>
                         <a href="/atletas">🏃‍♂️ Atletas</a>
-
                     </li>
-                     <li>
+                    <li>
                         <a href="/entrenadores">👨‍🏫 Entrenadores</a>
-
                     </li>
-                     <li>
+                    <li>
                         <a href="/planes">📋 Planes</a>
-
                     </li>
-                     <li>
+                    <li>
                         <a href="/pruebas">✅ Pruebas</a>
-
                     </li>
-                     <li>
+                    <li>
                         <a href="/login">🔐 Login</a>
-
                     </li>
+                </ul>
             </nav>
 
         </div>
